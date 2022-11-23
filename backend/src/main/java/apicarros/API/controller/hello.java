85ae5737da9aec60cb7ae1ab8ddaca5069e4f1a5
Gabriel <01gabrielribeiro01@gmail.com>
2022-11-23 10:05:47 -0300

    iniciando

---
+++ backend/src/main/java/apicarros/API/controller/hello.java
@@ -0,0 +1,21 @@
+package apicarros.API.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class hello {
+
+
+    @GetMapping("/")
+    public String hello()
+        {
+            return "Ola mundo"+ new Date();
+        }
+
+}
