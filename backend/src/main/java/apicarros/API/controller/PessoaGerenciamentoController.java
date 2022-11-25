48186ce9d899f917c695f398939e547de3397c3d
Gabriel <01gabrielribeiro01@gmail.com>
2022-11-24 22:19:50 -0300

    login

--- backend/src/main/java/apicarros/API/controller/PessoaGerenciamentoController.java
+++ backend/src/main/java/apicarros/API/controller/PessoaGerenciamentoController.java
@@ -2,8 +2,14 @@ package apicarros.API.controller;
 
 import apicarros.API.dto.PessoaUsuarioRequestDTO;
 import apicarros.API.entity.Pessoa;
+import apicarros.API.security.JwtUtil;
 import apicarros.API.service.PessoaGerenciamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +20,11 @@ public class PessoaGerenciamentoController {
     @Autowired
     private PessoaGerenciamentoService pessoaGerenciamentoService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
     @PostMapping("/recuperar-codigo")
     public String recuperarCodigo(@RequestBody Pessoa pessoa)
     {
@@ -23,6 +34,17 @@ public class PessoaGerenciamentoController {
     public String alterarSenha(@RequestBody Pessoa pessoa)
     {
         return pessoaGerenciamentoService.alterarSenha(pessoa);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Pessoa pessoa)
+    {
+        Authentication  authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(pessoa.getEmail(),pessoa.getSenha()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Pessoa autenticado = (Pessoa) authentication.getPrincipal();
+        String token =jwtUtil.gerarTokenUsername(autenticado);
+        return ResponseEntity.ok(token);
     }
 
 
